c7d3a521ae30a12f4a858a51612a04bb6519ccf6
gowthamreddy308 <g.gowthamreddy4u@gmail.com>
2019-10-16 15:18:42 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form className="Openform">
+          <div className="form-group">
+            <label htmlFor="fname">First Name</label>
+            <input type="text" className="form-control" id="fname" aria-describedby="fname"
+                   placeholder="Enter First name" />
+
+          </div>
+          <div className="form-group">
+            <label htmlFor="lname">Last Name</label>
+            <input type="text" className="form-control" id="lname" aria-describedby="lname"
+                   placeholder="Enter Last name" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                   placeholder="Enter Email"/>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          </div>
+
+          <button type="submit" className="btn">Register</button>
+        </form>
+
       </header>
     </div>
   );
